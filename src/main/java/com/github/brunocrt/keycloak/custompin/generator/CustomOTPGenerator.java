2a28da498ac50345bd664432b9cbdc3589f2d167
Bruno Tinoco <boto@gft.com>
2020-08-10 17:13:34 -0300

    adding a truncated version of the result hash

--- src/main/java/com/github/brunocrt/keycloak/custompin/generator/CustomOTPGenerator.java
+++ src/main/java/com/github/brunocrt/keycloak/custompin/generator/CustomOTPGenerator.java
@@ -13,7 +13,6 @@ public class CustomOTPGenerator implements PINGenerator {
     private final String HASH_ALGO = "HmacSHA256";
     private final String DEFAULT_TIME_FORMAT = "HH:mm";
 
-
     private String timeFormat;
 
     public CustomOTPGenerator() {
@@ -24,9 +23,62 @@ public class CustomOTPGenerator implements PINGenerator {
         this.timeFormat = timeFormat;
     }
 
+
     @Override
     public String generate(String seed, String data) throws PINGeneratorException {
+        return enforcedHMAC(seed, data, 8);
+    }
 
+    /**
+     * This generates a modified version of HMAC enforcing special characters.
+     * @param seed password seed (salt)
+     * @param data original data to be encrypted
+     * @param length length of the result code
+     * @return 8 characters message code
+     */
+    public String enforcedHMAC(String seed, String data, int length) throws PINGeneratorException {
+        final String originalHMAC = generateHMAC(seed, data);
+        for(int i=0; i<length; i++) {
+            if(!Character.isDigit(originalHMAC.charAt(i)) &&
+                    !Character.isLetter(originalHMAC.charAt(i))) {
+                if(i > length) {
+                    return originalHMAC.substring(0, length - 1) + originalHMAC.charAt(i);
+                } else {
+                    return originalHMAC.substring(0, length);
+                }
+            }
+        }
+
+        return originalHMAC.substring(0, length - 1) + "!";
+    }
+
+    /**
+     * Generates a reduced password according to a specified length parameter
+     * @param seed password seed (salt)
+     * @param data original data to be encrypted
+     * @param length the length of the encrypted password result. Minimal 6 characters.
+     * @return
+     */
+    public String truncatedHMAC(String seed, String data, int length) throws PINGeneratorException {
+        String originalPIN = generateHMAC(seed, data);
+        if(length > 3 || length < originalPIN.length()) {
+            return originalPIN.substring(0, length);
+        } else {
+            return originalPIN;
+        }
+    }
+
+    /**
+     * Generates an One Time Based Password (OTP) using HMAC 256 (SHA1) Algorithm.
+     * This combines the provided seed with a time sensitive timestamp with 1 minute
+     * of duration to encrypt the provided data.
+     *
+     * @param seed salt
+     * @param data data to be encoded
+     * @return Base 64 encoded password
+     * @throws PINGeneratorException
+     */
+    public String generateHMAC(String seed, String data) throws PINGeneratorException {
         if(seed == null || data == null)
             throw new PINGeneratorException("All input data is required to generate the PIN");
 
@@ -47,4 +99,5 @@ public class CustomOTPGenerator implements PINGenerator {
             throw new PINGeneratorException(e.getMessage());
         }
     }
+
 }
